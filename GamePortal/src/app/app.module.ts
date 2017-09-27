777d60e1a11a7c6b9189963a2e8bf3a8b8dfdd6b
Lei Guo <lg2681@nyu.edu>
2017-09-27 17:38:40 -0400

    need to implement database transaction

--- GamePortal/src/app/app.module.ts
+++ GamePortal/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBrfL2oO_3MCC-A9ympGKJGnZl2SERK7kA',
+  authDomain: 'gameportalangular.firebaseapp.com',
+  databaseURL: 'https://gameportalangular.firebaseio.com',
+  projectId: 'gameportalangular',
+  storageBucket: 'gameportalangular.appspot.com',
+  messagingSenderId: '678129730438'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
