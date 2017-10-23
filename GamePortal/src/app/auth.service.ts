5c80521b680649a2a13e613966091e2eb7f5f1da
ceciliazhang12 <ceciliazhang1231@gmail.com>
2017-10-22 23:26:02 -0400

    updated google login to adapt new rules when storing user info

--- GamePortal/src/app/auth.service.ts
+++ GamePortal/src/app/auth.service.ts
@@ -8,23 +8,48 @@ import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthService {
-  public user: Observable<firebase.User>;
+  // public user: Observable<firebase.User>;
+  public authState: any;
   items: AngularFireList<any>;
   errMessage = '';
   msg = '';
 
   constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase, private router: Router) {
-    this.user = this.afAuth.authState;
+    this.afAuth.authState.subscribe((auth) => {
+      this.authState = auth;
+    });
     this.items = af.list('items');
   }
 
+  createUserInfo(result: any): any {
+
+    const userInfo = {
+      'publicFields': {
+        'avatarImageUrl': (result.user.photoURL || ''),
+        'displayName':  (result.user.displayName || ''),
+        'isConnected':  true,
+        'lastSeen':  firebase.database.ServerValue.TIMESTAMP,
+      },
+      'privateFields' : {
+        'email':  (result.user.email || ''),
+        'createdOn':  firebase.database.ServerValue.TIMESTAMP,
+        'phoneNumber': '',
+        'facebookId': '',
+        'googleId': result.user.email,
+        'twitterId': '',
+        'githubId': '',
+        'friends': '',
+        'pushNotificationsToken': '',
+      }
+    };
+    return userInfo;
+  }
+
   logInWithGoogle() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function(result) {
-      firebase.database().ref('users/' + this.user.uid + '/publicFileds').set({
-        avatarImageUrl: this.user.photoURL,
-        displayName: this.user.displayName,
-        email: this.user.email
-      });
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(result => {
+      this.authState = result;
+      const userInfo = this.createUserInfo(result);
+      firebase.database().ref('users/' + result.user.uid).update(userInfo);
       console.log('success');
     })
       .catch(error => {
@@ -49,8 +74,8 @@ export class AuthService {
     this.afAuth.auth.signInAnonymously();
   }
 
-  Send(desc: string) {
-    this.items.push({ message: desc});
-    this.msg = '';
-  }
+  // Send(desc: string) {
+  //   this.items.push({ message: desc});
+  //   this.msg = '';
+  // }
 }
