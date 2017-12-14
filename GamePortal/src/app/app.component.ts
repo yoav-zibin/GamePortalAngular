c5fda0936110adc3c20605efc2ff33fe9471a4b0
ceciliazhang12 <ceciliazhang1231@gmail.com>
2017-12-13 22:12:34 -0500

    minor fix on the nav

--- GamePortal/src/app/app.component.ts
+++ GamePortal/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
 import {Router} from '@angular/router';
 import {AuthService} from './services/auth.service';
 
@@ -12,12 +10,10 @@ import {AuthService} from './services/auth.service';
 })
 export class AppComponent {
   title = 'Game Portal';
-  public authState: any;
+  public user: any;
 
   constructor(public afAuth: AngularFireAuth, public router: Router, public authService: AuthService) {
-    this.afAuth.authState.subscribe((auth) => {
-      this.authState = auth;
-    });
+    this.user = this.afAuth.authState;
   }
 }
 
