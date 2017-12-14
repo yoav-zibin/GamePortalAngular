2477e8f5c2715568da22132b9e455267d0ece3b2
ceciliazhang12 <ceciliazhang1231@gmail.com>
2017-12-14 02:42:03 -0500

    fix parsing groupId parameter in play route

--- GamePortal/src/app/playground/playground.component.ts
+++ GamePortal/src/app/playground/playground.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SpecService} from '../services/spec.service';
 import {AngularFireAuth} from 'angularfire2/auth';
+import {ActivatedRoute} from '@angular/router';
+import {GroupService} from '../services/group.service';
 
 @Component({
   selector: 'app-playground',
@@ -9,8 +11,12 @@ import {AngularFireAuth} from 'angularfire2/auth';
 })
 export class PlaygroundComponent implements OnInit {
   public user: any;
-  constructor(private specService: SpecService, private afAuth: AngularFireAuth) {
+  constructor(private specService: SpecService,
+              private afAuth: AngularFireAuth,
+              private groupService: GroupService,
+              private route: ActivatedRoute) {
     this.user = this.afAuth.authState;
+    this.route.params.subscribe(params => this.groupService.setGroupID(params['groupId']));
   }
 
   ngOnInit() {
